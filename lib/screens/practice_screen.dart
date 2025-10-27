d0c373fb65d060dd3d00824ebba542a82b094c27
ZhongTheKong <guoyizhong2004@gmail.com>
2025-10-27 18:02:41 -0400

    Re-added record button

--- lib/screens/practice_screen.dart
+++ lib/screens/practice_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:read_right_project/models/record_button.dart';
 import 'package:read_right_project/utils/routes.dart';
 
 class PracticeScreen extends StatelessWidget {
@@ -16,6 +17,8 @@ class PracticeScreen extends StatelessWidget {
               'Hello, World! This is the Practice Screen.',
               style: TextStyle(fontSize: 18),
             ),
+            const SizedBox(height: 20),
+            RecordButton(),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
