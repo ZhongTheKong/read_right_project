ea8979fb37487528eaea93ea92da6905b5ed5b2a
ZhongTheKong <guoyizhong2004@gmail.com>
2025-10-31 16:41:11 -0400

    Work on login persistence with shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:read_right_project/providers/recording_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'utils/routes.dart';
 
 void main() {
@@ -19,8 +20,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Navigation Demo',
       debugShowCheckedModeBanner: false,
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:read_right_project/data/login_data.dart';
 import 'package:read_right_project/models/labeled_login_text_field.dart';
 import 'package:read_right_project/models/login_text_field.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,73 +12,102 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  Future<String> getLastLoggedInUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? lastUser = prefs.getString("lastUser");
+    return lastUser ?? "";
+  }
+
   @override
   Widget build(BuildContext context) {
 
     TextEditingController usernameTextEditingController = TextEditingController();
     TextEditingController passwordTextEditingController = TextEditingController();
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Login Screen')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Hello, World! This is the Login Screen.',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 20),
+    return FutureBuilder<String?>(
+      future: getLastLoggedInUsername(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          final String lastLoggedInUsername = snapshot.data ?? '';
+          if (lastLoggedInUsername == '') {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Login Screen')),
+              body: Center(
+                child: Text("Welcome back: $lastLoggedInUsername"),
+              )
+            );
+          }
+          else {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Login Screen')),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Hello, World! This is the Login Screen.',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(height: 20),
 
 
 
-            const Text(
-              'Select Between Student Or Teacher',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 20),
-            
-            LabelledLoginTextField(
-              textEditingController: usernameTextEditingController, 
-              fieldIcon: Icons.face, 
-              labelText: "Username"
-            ),
-            const SizedBox(height: 20),
+                    const Text(
+                      'Select Between Student Or Teacher',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(height: 20),
+                    
+                    LabelledLoginTextField(
+                      textEditingController: usernameTextEditingController, 
+                      fieldIcon: Icons.face, 
+                      labelText: "Username"
+                    ),
+                    const SizedBox(height: 20),
 
-            LabelledLoginTextField(
-              textEditingController: passwordTextEditingController, 
-              fieldIcon: Icons.key, 
-              labelText: "Password"
-            ),
-            const SizedBox(height: 20),
+                    LabelledLoginTextField(
+                      textEditingController: passwordTextEditingController, 
+                      fieldIcon: Icons.key, 
+                      labelText: "Password"
+                    ),
+                    const SizedBox(height: 20),
 
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to main screen and clear previous routes
-                // Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-                if (LoginData.isValidLoginData(usernameTextEditingController.text, passwordTextEditingController.text) == true)
-                {
-                  print("successful login");
-                }
-                else
-                {
-                  print("incorrect login");
-                }
-              },
-              child: const Text('LOGIN'),
-            ),
-            const SizedBox(height: 20),
-            
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to main screen and clear previous routes
-                Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-              },
-              child: const Text('Back to Main Screen'),
-            ),
-          ],
-        ),
-      ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navigate back to main screen and clear previous routes
+                        // Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+                        if (LoginData.isValidLoginData(usernameTextEditingController.text, passwordTextEditingController.text) == true)
+                        {
+                          print("successful login");
+                        }
+                        else
+                        {
+                          print("incorrect login");
+                        }
+                      },
+                      child: const Text('LOGIN'),
+                    ),
+                    const SizedBox(height: 20),
+                    
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navigate back to main screen and clear previous routes
+                        Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+                      },
+                      child: const Text('Back to Main Screen'),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        }
+      },
     );
   }
 }
