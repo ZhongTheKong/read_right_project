64f2859b5ac66fbcc1898164266266ed278ecc27
ZhongTheKong <guoyizhong2004@gmail.com>
2025-11-03 18:31:52 -0500

    Feedback screen design

--- lib/screens/feedback_screen.dart
+++ lib/screens/feedback_screen.dart
@@ -8,31 +8,92 @@ class FeedbackScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    RecordingProvider recordingProvider = context.watch<RecordingProvider>();
+    RecordingProvider recordingProvider = context.read<RecordingProvider>();
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Feedback Screen')),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Feedback')
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Hello, World! This is the Feedback Screen.',
+
+            // Container(
+              // width: 500,
+              // color: Colors.blue[50],
+              // child: Row(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.green[50],
+                    padding: EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Word',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        const SizedBox(height: 20),
+                                  
+                        Text(
+                          recordingProvider.attempts.isNotEmpty 
+                            ? recordingProvider.attempts[recordingProvider.index].word
+                            : '???',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        
+                      ],
+                    ),
+                  ),
+              
+                  const SizedBox(width: 20),
+              
+                  Container(
+                    color: Colors.orange[50],
+                    padding: EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Date',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        const SizedBox(height: 20),
+                                  
+                        Text(
+                          recordingProvider.attempts.isNotEmpty 
+                            ? '${recordingProvider.attempts[recordingProvider.index].createdAt.toLocal()}'
+                            : '???',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                ],
+              ),
+            // ),
+            
+
+            
+
+
+            Text(
+              recordingProvider.attempts.isNotEmpty 
+                ? 'Score: ${recordingProvider.attempts[recordingProvider.index].score}' 
+                : 'Score: ???',
               style: TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 20),
 
 
-            Text(
-              recordingProvider.attempts.isNotEmpty ? 'Score: ${recordingProvider.attempts[recordingProvider.index].score}' : 'Score: ???',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 20),
-
-
 
             Text(
-              recordingProvider.attempts.isNotEmpty ? 'Feedback: ${recordingProvider.attempts[recordingProvider.index].score}' : 'Score: ???',
+              recordingProvider.attempts.isNotEmpty 
+                ? 'Feedback: ${recordingProvider.attempts[recordingProvider.index].score}' 
+                : 'Feedback: ???',
               style: TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 20),
