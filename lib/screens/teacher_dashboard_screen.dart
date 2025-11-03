64b99fa72c7115dc5913988f751c798d480ffc73
Reno117 <parkerrenoestes@gmail.com>
2025-11-02 19:57:07 -0500

    Changed teacher_dashboard to be a test for voice input (temporary)

--- lib/screens/teacher_dashboard_screen.dart
+++ lib/screens/teacher_dashboard_screen.dart
@@ -27,7 +27,6 @@ class TeacherDashboardScreen extends StatelessWidget {
                   ),
                   const SizedBox(height: 10),
 
-                  // Transcription status
                   Text(
                     provider.isTranscribing
                         ? 'Transcribing...'
