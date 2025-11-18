334a8162eb246580b2aee992f2125d854317a52a
ZhongTheKong <guoyizhong2004@gmail.com>
2025-11-17 21:57:02 -0500

    Merge branch 'main' of https://github.com/ZhongTheKong/read_right_project

--- lib/screens/teacher_dashboard_screen.dart
+++ lib/screens/teacher_dashboard_screen.dart
@@ -34,11 +34,11 @@ class _TeacherDashboardScreenState extends State<TeacherDashboardScreen> {
   }
   */
 
-  final lists = ['Math', 'Science', 'History'];
+  final lists = ['1st Grade', '2nd Grade', '3rd Grade'];
   final classOverview = [
-    {'name': 'Alice', 'list': 'Math', 'date': '2025-11-15'},
-    {'name': 'Bob', 'list': 'Science', 'date': '2025-11-16'},
-    {'name': 'Charlie', 'list': 'History', 'date': '2025-11-17'},
+    {'name': 'Alice', 'list': '1st Grade', 'date': '2025-11-15'},
+    {'name': 'Bob', 'list': '2nd Grade', 'date': '2025-11-16'},
+    {'name': 'Charlie', 'list': '3rd Grade', 'date': '2025-11-17'},
   ];
 
   // Filter function
